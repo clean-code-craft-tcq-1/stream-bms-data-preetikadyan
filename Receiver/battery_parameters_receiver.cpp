a50a72e55f8348acd36a283f210cd32b9b0dbbee
Akshay3012 <77444334+Akshay3012@users.noreply.github.com>
2021-06-20 23:30:33 +0530

    Update battery_parameters_receiver.cpp

--- Receiver/battery_parameters_receiver.cpp
+++ Receiver/battery_parameters_receiver.cpp
@@ -12,61 +12,104 @@ void Battery_Parameter_Receiver::receive_data_from_console()
     
      std::getline(std::cin, sender_data_heading);
      std::getline(std::cin, sender_data);
+     get_temperature_values(sender_data);
+     get_soc_values(sender_data);
+}
 
-    string temp_data,temp_data1,temp_data2;
-    
-
-    int temp_arr[6],soc_arr[6];
-    int count_temp,count_soc= 0;
-    int i;
+void Battery_Parameter_Receiver::get_temperature_values(string sender_data)
+{
+    string temp_data;
+    int count_temp=0;
     int n = sender_data.length() ;
-    std::cout <<"sender data is" <<sender_data << std::endl;
-    std::cout << "length of data is "<< sender_data.length() << std::endl;
-    std::cout << "first char of data is "<< sender_data[0] << std::endl;	
-    for( i=0; i < n; i++)
+    
+    for(int i=0; i < n; i++)
     {
         if((sender_data[i] == 't') && (sender_data[i+1] == 'e') && (sender_data[i+2] == 'm'))
         {
-           std::cout << "first t is "<< i << std::endl;
-            if(i < (n-16))
-            {
               i=i+16;
-              temp_data1 = sender_data[i];
-              temp_data2 = sender_data[i+1];
-              temp_data = temp_data1 + temp_data2;
-	      std::cout << "temp_data is "<< temp_data << std::endl;
-              temp_arr[count_temp] = std::stoi(temp_data);
-	      parameter.Temperrature = temp_arr[count_temp]; 
-	      std::cout << "temp value in parameter class is "<< parameter.Temperrature << std::endl;
-	     // printf("temp value in parameter is %f \n", parameter.Temperrature);
+              temp_data[0] =sender_data[i];
+              temp_data[1] =sender_data[i+1];
+	          temperature_values.push_back(std::stoi(temp_data)); 
               count_temp++;
-            }
         }
-     //   else if(sender_data[i] == 'S')
-     //   {
-       //     if(i < (n-8))
-         //   {
-           //   i=i+7;
-            //  temp_data1 = sender_data[i];
-              //temp_data2 = sender_data[i+1];
-              //temp_data = temp_data1 + temp_data2;
-             // soc_arr[count_soc] = std::stoi(temp_data);
-              //count_soc++;
-           // }
-       // }
+       
     }
-     // for(int i =0;i<count_temp;i++)
-     //   {
-          //  std::cout << "temperature " << i << " value: " << temp_arr[i] << std::endl;
-	   // parameter.Temperrature = temp_arr[i];
-	    
-      //  }
-      //   for(int i =0;i<count_soc;i++)
-     //   {
-          //  std::cout << "SOC " << i << " value: " << soc_arr[i] << std::endl;
-           // parameter.SOC = soc_arr[i];
-      //  }
-	
+   std::cout << "temp values are: "<<temperature_values[0] << ',' << temperature_values[1] << ',' << temperature_values[2] << ',' << temperature_values[3] << ',' << temperature_values[4] << ',' << temperature_values[5] << std::endl;
+    calculate_parameter_max(temperature_values);
+    calculate_parameter_min(temperature_values);
+    calculate_parameter_avg(temperature_values);
+}
+
+void Battery_Parameter_Receiver::get_soc_values(string data)
+{
+    string soc_data;
+    int count_soc=0;
+    int n = data.length() ;
+    
+    for(int i=0; i < n; i++)
+    {
+        if((data[i] == 's') && (data[i+1] == 't'))
+        {
+              i=i+20;
+              soc_data[0] = data[i];
+              soc_data[1] = data[i+1];
+	          soc_values.push_back(std::stoi(soc_data)); 
+              count_soc++;
+        }
+       
+    }
+   std::cout << "temp values are: "<<soc_values[0] << ',' << soc_values[1] << ',' << soc_values[2] << ',' << soc_values[3] << ',' << soc_values[4] << ',' << soc_values[5] << std::endl;
+    calculate_parameter_max(soc_values);
+    calculate_parameter_min(soc_values);
+    calculate_parameter_avg(soc_values);
+}
+
+int Battery_Parameter_Receiver::calculate_parameter_max(vector<int> parameter_values)
+{
+    int max = parameter_values[0];
+    for(int i =0; i<6;i++)
+    {
+        if(parameter_values[i] > max)
+        {
+            max = parameter_values[i];
+            
+        }
+    }
+ 
+    std::cout << "min Temp value "<< max  << std::endl;
+    return max;
+}
+
+int Battery_Parameter_Receiver::calculate_parameter_min(vector<int> parameter_values)
+{
+    int min = parameter_values[0] ;
+    for(int i =0; i<6;i++)
+    {
+        if(parameter_values[i] < min)
+        {
+            min = parameter_values[i];
+            
+        }
+    }
+ 
+    std::cout << "min Temp value "<< min  << std::endl;
+    return min;
+}
+
+int Battery_Parameter_Receiver::calculate_parameter_avg(vector<int> parameter_values)
+{
+    int count = 6 ;
+    int n = sizeof(parameter_values)/sizeof(parameter_values[0]);
+     std::cout << "size "<< n  << std::endl;
+    int sum; int avg;
+    for(int i =0; i<6;i++)
+    {
+        sum = parameter_values[n]+ parameter_values[n-1]+parameter_values[n-2]+parameter_values[n-3]+parameter_values[n-4];             
+    }
+   
+    avg = sum/count;
+    std::cout << "avgvalue "<< avg  << std::endl;
+    return avg;
 }
 
 
