1f933938b925c1fd8348dfa315d256f4a43993a5
Akshay3012 <77444334+Akshay3012@users.noreply.github.com>
2021-06-20 15:52:14 +0530

    Update battery_parameters_receiver_test.cpp

--- Receiver/battery_parameters_receiver_test.cpp
+++ Receiver/battery_parameters_receiver_test.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+#include "battery_parameters_receiver.h"
+using namespace std;
 
+int main()
+{
+	Battery_Parameter_Receiver bp_receiver;
+	
+	bp_receiver.receive_data_from_console();
+
+	return 0;
+}
