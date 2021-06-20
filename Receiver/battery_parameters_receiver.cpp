c09c7cc6403a262f3800d32c3936178b4feb2ddd
Akshay3012 <77444334+Akshay3012@users.noreply.github.com>
2021-06-20 17:44:02 +0530

    Update battery_parameters_receiver.cpp

--- Receiver/battery_parameters_receiver.cpp
+++ Receiver/battery_parameters_receiver.cpp
@@ -1,9 +1,6 @@
-#include <boost/property_tree/ptree.hpp>
-#include <boost/property_tree/json_parser.hpp>
-#include <boost/foreach.hpp>
 #include "battery_parameters_receiver.h"
+using namespace std;
 
-using boost::property_tree::ptree;
 
 void Battery_Parameter_Receiver::receive_data_from_console()
 {
@@ -13,31 +10,55 @@ void Battery_Parameter_Receiver::receive_data_from_console()
 	{
 		Parameters parameter;
 		std::getline(std::cin, sender_data);
+	}
+	
+	    string temp_data,temp_data1,temp_data2;
 
-		if (parse_data(sender_data, parameter))
-			parameter_list.push_back(parameter);
-	}	
+    int temp_arr[3],soc_arr[3];
+    int count_temp,count_soc= 0;
+    int i;
+    int n = sender_data.length() ;
+    std::cout << sender_data << std::endl;
+    std::cout << sender_data.length() << std::endl;
+    for( i=0; i < n; i++)
+    {
+        if((sender_data[i] == 't') && (sender_data[i+1] == 'e'))
+        {
+            if(i < (n-16))
+            {
+              i=i+15;
+              temp_data1 = sender_data[i];
+              temp_data2 = sender_data[i+1];
+              //if(sender_data[i+2] != ',')
+              //{
+                  //temp_data3 = sender_data[i+2];
+              //}
+              temp_data = temp_data1 + temp_data2;
+              temp_arr[count_temp] = stoi(temp_data);
+              count_temp++;
+            }
+        }
+        else if(sender_data[i] == 'S')
+        {
+            if(i < (n-8))
+            {
+              i=i+7;
+              temp_data1 = sender_data[i];
+              temp_data2 = sender_data[i+1];
+              temp_data = temp_data1 + temp_data2;
+              soc_arr[count_soc] = stoi(temp_data);
+              count_soc++;
+            }
+        }
+    }
+      for(int i =0;i<count_temp;i++)
+        {
+            std::cout << "temperature " << i << " value: " << temp_arr[i] << std::endl;
+        }
+         for(int i =0;i<count_soc;i++)
+        {
+            std::cout << "SOC " << i << " value: " << soc_arr[i] << std::endl;
+        }
 }
 
-bool Battery_Parameter_Receiver::parse_data(string data, Parameters &parameter)
-{
-	std::stringstream ss;
-	ss.str(data);
-
-	try
-	{
-		ptree pt;
-		boost::property_tree::read_json(ss, pt);
-
-		parameter.json_string = data;
-		parameter.Temperrature = pt.get<float>("Temperature");
-		parameter.SOC = pt.get<float>("SOC");
-	}
-	catch (std::exception const& e)
-	{
-		std::cerr << e.what() << std::endl;
-		return false;
-	}
-
-}
 
