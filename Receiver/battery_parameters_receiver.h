4133ed24ca3850f41ceb948a6883188dceea814f
Akshay3012 <77444334+Akshay3012@users.noreply.github.com>
2021-06-20 15:52:07 +0530

    Update battery_parameters_receiver.h

--- Receiver/battery_parameters_receiver.h
+++ Receiver/battery_parameters_receiver.h
@@ -1,1 +1,22 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+typedef struct
+{
+	string json_string;
+	double SOC;
+	double Temperrature;
+}Parameters;
+
+
+class Battery_Parameter_Receiver
+{
+	vector<Parameters> parameter_list;
+	
+	void receive_data_from_console();
+	bool parse_data(string data, Parameters &parameter);
+};
 
