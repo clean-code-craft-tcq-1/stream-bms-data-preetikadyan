81d8d5bae9e1d19dc6a8559e30b00d57d11181c4
Akshay3012 <77444334+Akshay3012@users.noreply.github.com>
2021-06-21 16:35:56 +0530

    Update battery_parameters_receiver.h

--- Receiver/battery_parameters_receiver.h
+++ Receiver/battery_parameters_receiver.h
@@ -10,10 +10,21 @@ class Battery_Parameter_Receiver
 	vector<int> soc_values;
 	
 	public:
+	// get data via pipe from console which is written by Sender
 	void get_data_from_console();
+	
+	//Extract Temperature values from Sender data
 	void get_temperature_values(string sender_data);
+	
+	// Extract SOC Values from Sender data
 	void get_soc_values(string sender_data);
+	
+	// Calculate maximum value for a parameter of input stream
 	int calculate_parameter_max(vector<int> parameter_values, string parameter_type);
+	
+	// Calculate minimum value for a parameter of input stream
 	int calculate_parameter_min(vector<int> parameter_values, string parameter_type);
-	int calculate_parameter_avg(vector<int> parameter_values, string parameter_type);
+	
+	// Calculate Simple moving average value for last 5 values in the input stream of a parameter
+	int calculate_parameter_sma(vector<int> parameter_values, string parameter_type);
 };
