6829accf476d6bd6f61fa3d2b0a0c9df97584388
Akshay3012 <77444334+Akshay3012@users.noreply.github.com>
2021-06-21 16:08:05 +0530

    Update battery_parameters_receiver_test.cpp

--- Receiver/battery_parameters_receiver_test.cpp
+++ Receiver/battery_parameters_receiver_test.cpp
@@ -9,7 +9,7 @@ TEST_CASE(" Received Parameter values statistics")
      	vector<int> parameter_values = {10,15,89,2,56,67};
 	string parameter_type = "Temperature";
 	Battery_Parameter_Receiver obj_Battery_Parameter_Receiver;
-	EXPECT_EQ(2, obj_Battery_Parameter_Receiver.calculate_parameter_min(parameter_values,parameter_type));
+	assert(obj_Battery_Parameter_Receiver.calculate_parameter_min(parameter_values,parameter_type) == 2);
  
 }
 
