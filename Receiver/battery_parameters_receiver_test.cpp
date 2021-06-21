a895ad3707326d304084ccfcea900038669d3d5b
Akshay3012 <77444334+Akshay3012@users.noreply.github.com>
2021-06-21 16:35:49 +0530

    Update battery_parameters_receiver_test.cpp

--- Receiver/battery_parameters_receiver_test.cpp
+++ Receiver/battery_parameters_receiver_test.cpp
@@ -6,10 +6,31 @@ using namespace std;
 
 TEST_CASE(" Received Parameter values statistics")
 {
-     	vector<int> parameter_values = {10,15,89,2,56,67};
+	// Temperature values from Sender data
+     	vector<int> parameter_values = {10,15,89,2,56,68};
 	string parameter_type = "Temperature";
+	
+	// Object creation of Class Battery_Parameter_Receiver
 	Battery_Parameter_Receiver obj_Battery_Parameter_Receiver;
+	
+	// Check for Temperature minimum value
 	assert(obj_Battery_Parameter_Receiver.calculate_parameter_min(parameter_values,parameter_type) == 2);
+	// Check for Temperature maximum value
+	assert(obj_Battery_Parameter_Receiver.calculate_parameter_max(parameter_values,parameter_type) == 89);
+	// Check for Simple moving average value for last 5 values of input stream
+	assert(obj_Battery_Parameter_Receiver.calculate_parameter_sma(parameter_values,parameter_type) == 46);
+	
+	
+	// SOC values from Sender data
+     	vector<int> parameter_values = {45,3,99,84,66,18};
+	string parameter_type = "SOC";
+	
+	// Check for Temperature minimum value
+	assert(obj_Battery_Parameter_Receiver.calculate_parameter_min(parameter_values,parameter_type) == 3);
+	// Check for Temperature maximum value
+	assert(obj_Battery_Parameter_Receiver.calculate_parameter_max(parameter_values,parameter_type) == 99);
+	// Check for Simple moving average value for last 5 values of input stream
+	assert(obj_Battery_Parameter_Receiver.calculate_parameter_sma(parameter_values,parameter_type) == 54);
  
 }
 
