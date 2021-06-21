35202c6c232c31f7c86f80ba537e2728bd36823d
Akshay3012 <77444334+Akshay3012@users.noreply.github.com>
2021-06-21 16:46:04 +0530

    Update battery_parameters_receiver_test.cpp

--- Receiver/battery_parameters_receiver_test.cpp
+++ Receiver/battery_parameters_receiver_test.cpp
@@ -35,3 +35,12 @@ TEST_CASE(" Received Parameter values statistics")
 }
 
 
+// To Print the Input stream from Console
+int main()
+{
+	Battery_Parameter_Receiver bp_receiver;
+        bp_receiver.get_data_from_console();
+        return 0;
+}
+
+
